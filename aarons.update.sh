54c6e55ceb473a26d8809cba2de3bbb822ce016d
Aaron Decker <aaron@ard.ninja>
2016-03-14 17:05:24 -0400

    update

--- aarons.update.sh
+++ aarons.update.sh
@@ -1,5 +1,5 @@
 cd ~/.vim_runtime && python update_plugins.py && cd ..
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-cp ~/dotfiles/nix/.vim_runtime/my_configs.vim ~/.vim_runtime/my_configs.vim
+cd ~/.vim_runtime
 
 
